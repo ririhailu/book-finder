c66ba8cdd998d8bd39e2058c38d68140cce6f28b
Matrix <rhailu88@gmail.com>
2023-01-26 10:38:03 -0500

    Set state for form validation and alert in components file.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -11,3 +11,30 @@ const SignupForm = () => {
     email: '', 
     password: '' 
   });
+
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserFormData({
+      ...userFormData,
+      [name]: value 
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
