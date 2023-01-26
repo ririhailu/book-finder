b9a3fbfd9b6a3c0843a66db776d2f124330f6aa2
Matrix <rhailu88@gmail.com>
2023-01-26 10:48:15 -0500

    Added all import in components file.

---
+++ client/src/pages/SavedBooks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+
+const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const userData = data?.me || {};
